670f008de5ced23b0846abff7c3f6ce4edd5789a
mohamedelaraby <mohamedelaraby1995@gmail.com>
2020-07-21 09:22:13 +0200

    Refactoring admin section

--- resources/views/admin/admins/create.blade.php
+++ resources/views/admin/admins/create.blade.php
@@ -7,7 +7,7 @@
     <div class="row">
       <div class="col-12">
         <div class="card">
-          
+
           {{-- @include('admin.layouts.message')  --}}
 
           <div class="card-header">
@@ -16,40 +16,7 @@
           <!-- /.card-header -->
           <div class="card-body">
             {!! Form::open(['route'=>'admin.store']) !!}
-            {!! Form::token() !!}
-            {{-- name --}}
-            <div class="form-group">
-              {!! Form::label('name', trans('admin.name')) !!}
-              {!! Form::text('name',old('name'),
-                           [ 'class' =>'form-control',
-                            // 'required' =>true,
-                            'placeholder' =>trans('admin.name'),
-                            'auto-focus'=>'true' ]) 
-              !!}
-            </div>
-            
-            {{-- Email --}}
-            <div class="form-group">
-              {!! Form::label('email', trans('admin.email')) !!}
-              {!! Form::email('email',old('email'),
-                           [ 'class' =>'form-control',
-                            // 'required' =>true,
-                            'placeholder' =>trans('admin.email'),
-                            'auto-focus'=>'true' ]) 
-              !!}
-            </div>
-            
-            {{-- password --}}
-            <div class="form-group">
-              {!! Form::label('password', trans('admin.password')) !!}
-              {!! Form::password('password',
-                           [ 'class' =>'form-control',
-                            // 'required' =>true,
-                            'placeholder' =>trans('admin.password'),
-                            'auto-focus'=>'true' ]) 
-              !!}
-            </div>
-
+           @inlcude('admin.admins.form')
             {!! Form::submit(trans('admin.create_admin'), ['class' => 'btn btn-info']) !!}
             {!! Form::close() !!}
           </div>
--- resources/views/admin/admins/edit.blade.php
+++ resources/views/admin/admins/edit.blade.php
@@ -7,7 +7,7 @@
     <div class="row">
       <div class="col-12">
         <div class="card">
-          
+
           {{-- @include('admin.layouts.message')  --}}
 
           <div class="card-header">
@@ -16,42 +16,9 @@
           <!-- /.card-header -->
           <div class="card-body">
             {!! Form::open(['route'=>['admin.update',$admin->id], 'method' =>'post']) !!}
-            {!! Form::token() !!}
             {!! Form::hidden('_method','put') !!}
-            
-            {{-- name --}}
-            <div class="form-group">
-              {!! Form::label('name', trans('admin.name')) !!}
-              {!! Form::text('name',$admin->name,
-                           [ 'class' =>'form-control',
-                            // 'required' =>true,
-                            'placeholder' =>trans('admin.name'),
-                            'auto-focus'=>'true' ]) 
-              !!}
-            </div>
-            
-            {{-- Email --}}
-            <div class="form-group">
-              {!! Form::label('email', trans('admin.email')) !!}
-              {!! Form::email('email',$admin->email,
-                           [ 'class' =>'form-control',
-                            // 'required' =>true,
-                            'placeholder' =>trans('admin.email'),
-                            'auto-focus'=>'true' ]) 
-              !!}
-            </div>
-            
-            {{-- password --}}
-            <div class="form-group">
-              {!! Form::label('password', trans('admin.password')) !!}
-              {!! Form::password('password',
-                           [ 'class' =>'form-control',
-                            // 'required' =>true,
-                            'placeholder' =>trans('admin.password'),
-                            'auto-focus'=>'true' ]) 
-              !!}
-            </div>
 
+            @include('admin.admins.form')
             {!! Form::submit(trans('admin.save'), ['class' => 'btn btn-info']) !!}
             {!! Form::close() !!}
           </div>
